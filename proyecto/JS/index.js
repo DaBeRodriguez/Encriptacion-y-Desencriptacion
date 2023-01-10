8dedf908fdef96d9545ac98ba40f0c45a3de4592
DaBeRodriguez <midalove1419@gmail.com>
2023-01-10 09:42:34 -0500

    edite JS

--- proyecto/JS/index.js
+++ proyecto/JS/index.js
@@ -2,17 +2,23 @@ function ingresar() {
     var Passwordsistema = '12345678910'
     var Mensajeerror = 'Contraseña incorrecta'
     var password = document.getElementById("password").value;
+    var nombreUsser =document.getElementById("nombre").value;
 
     if (password == Passwordsistema) { 
         document.getElementById("error").innerHTML = " ";
         location.href ="opciones.html";
-
-
+        localStorage.setItem("nombre", nombreUsser);
+      
     } else {
         document.getElementById("error").innerHTML = Mensajeerror;
     }
-
 }
+
+function muestra(){
+    document.getElementById("saludo").innerHTML+=localStorage.getItem("nombre");
+}
+muestra();
+
 function redireccion1 (){
     location.href ="encriptar.html";
 }
@@ -37,4 +43,3 @@ function desencriptar(){
 
 
 }
-
