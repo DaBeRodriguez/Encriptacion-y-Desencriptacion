9a392113f6e961896af1c0eb99731a684bfd23d7
DaBeRodriguez <midalove1419@gmail.com>
2023-01-24 11:11:09 -0500

    al dia

---
+++ unidad3/array_busqueda/main.js
@@ -0,0 +1,14 @@
+//==========ARRAY INDEXOF============
+
+// let array=[2,9,5,2];
+// console.log(array.indexOf(2))
+
+// let array=[2,9,5,2];
+// console.log(array.lastOf(2));
+
+//==========ARRAY LASTINDEXOF============
+console.log([1,2,3].includes(2));
+console.log([1,2,3].includes(4));
+console.log([1,2,3].includes(3,3));
+console.log([1,2,3].includes(3,-1));
+console.log([1,2,NaN].includes(NaN));
